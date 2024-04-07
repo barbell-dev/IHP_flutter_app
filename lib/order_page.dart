52e5de6627fd76bfb280f2b0ad74156492b1774c
Varun Dunna <varun.dunna2524@gmail.com>
2024-04-07 13:14:39 +0530

    Added 'your orders' feature

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -10,6 +10,7 @@ class _OrderPageState extends State<OrderPage> {
   String? selectedVariant;
   int quantity = 1; // Default quantity is 1
   int bagCount = 0;
+  List<Order> orders = [];
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +23,7 @@ class _OrderPageState extends State<OrderPage> {
               IconButton(
                 icon: Icon(Icons.shopping_bag),
                 onPressed: () {
-                  // Add functionality for bag icon
+                  _showOrdersDialog();
                 },
               ),
               if (bagCount > 0)
@@ -116,10 +117,53 @@ class _OrderPageState extends State<OrderPage> {
               onPressed: selectedChocolate != null ? () => addToBag() : null,
               child: Text('Add to Bag'),
             ),
+            SizedBox(height: 16.0),
+            // Pricing Table
+            _buildPricingTable(),
+            SizedBox(height: 16.0),
+            // Checkout Button
+            ElevatedButton(
+              onPressed: bagCount > 0 ? () => checkout() : null,
+              child: Text('Checkout'),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildPricingTable() {
+    return DataTable(
+      columns: [
+        DataColumn(label: Text('Variant')),
+        DataColumn(label: Text('Price (INR)')),
+      ],
+      rows: _buildPricingRows(),
+    );
+  }
+
+  List<DataRow> _buildPricingRows() {
+    final Map<String, int> prices = {
+      '10g - Regular': 10,
+      '35g - Regular': 30,
+      '110g - Regular': 80,
+      '110g - Silk': 95,
+    };
+
+    List<DataRow> rows = [];
+    prices.forEach((variant, price) {
+      rows.add(DataRow(
+        cells: [
+          DataCell(Text(variant)),
+          DataCell(Row(
+            children: [
+              Text('\u{20B9}$price'),
+            ],
+          )),
+        ],
+      ));
+    });
+    return rows;
   }
 
   List<DropdownMenuItem<String>> _buildVariantItems(String? chocolate) {
@@ -129,7 +173,7 @@ class _OrderPageState extends State<OrderPage> {
       variants.addAll(
           ['10g - Regular', '35g - Regular', '110g - Regular', '110g - Silk']);
     } else {
-      variants.addAll(['10g', '35g', '110g']);
+      variants.addAll(['10g - Regular', '35g - Regular', '110g - Regular']);
     }
 
     return variants.map((String value) {
@@ -145,8 +189,96 @@ class _OrderPageState extends State<OrderPage> {
 
   void addToBag() {
     setState(() {
+      // Check if an order with the same chocolate and variant already exists
+      bool foundExistingOrder = false;
+      for (Order order in orders) {
+        if (order.chocolate == selectedChocolate &&
+            order.variant == selectedVariant) {
+          // Update the quantity of the existing order
+          order.quantity += quantity;
+          foundExistingOrder = true;
+          break;
+        }
+      }
+
+      // If no existing order found, add a new order
+      if (!foundExistingOrder) {
+        orders.add(Order(selectedChocolate!, selectedVariant!, quantity));
+      }
+
       // Increase bag count by the selected quantity
       bagCount += quantity;
     });
   }
+
+  void checkout() {
+    // Add functionality for checkout button
+  }
+
+  void _showOrdersDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'Your Orders',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  DataTable(
+                    columns: [
+                      DataColumn(label: Text('Chocolate')),
+                      DataColumn(label: Text('Variant')),
+                      DataColumn(label: Text('Quantity')),
+                    ],
+                    rows: orders.map((order) {
+                      return DataRow(
+                        cells: [
+                          DataCell(Text(order.chocolate)),
+                          DataCell(Text(order.variant)),
+                          DataCell(Text(order.quantity.toString())),
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class Order {
+  final String chocolate;
+  final String variant;
+  int quantity;
+
+  Order(this.chocolate, this.variant, this.quantity);
 }
