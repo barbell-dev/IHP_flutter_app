b2294fee4177691c90c7415176c587f1fc1e2006
Varun Dunna <varun.dunna2524@gmail.com>
2024-04-11 11:54:21 +0530

    otp functionality added to signup

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -16,18 +16,6 @@ class _SignupPageState extends State<SignupPage> {
   String _password = '';
   String _otp = '';
 
-  late TwilioFlutter twilioClient;
-
-  @override
-  void initState() {
-    super.initState();
-    twilioClient = TwilioFlutter(
-      accountSid: 'ACef4000326b184edc0aeccfa567a277db',
-      authToken: 'e4e9ddd7e33abda75fbaebbcc97fd4fb',
-      twilioNumber: '+1 205 236 0176', // Replace with your Twilio phone number
-    );
-  }
-
   bool _isPhoneNumberValid(String? value) {
     if (value == null || value.isEmpty) {
       return false;
